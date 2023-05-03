e19f93fc4db1c476106e31da79086091bb35b11e
Isabel <ifalcon@mail.sfsu.edu>
2023-05-02 23:03:15 -0700

    fixed calculations for bandwidth

--- benchmark.cpp
+++ benchmark.cpp
@@ -108,14 +108,24 @@ int main(int argc, char **argv) {
         std::cout << " Elapsed time: " << elapsed.count() << " " << std::endl;
         //n*n is matrix A bc it's a nxn matrix. x and y matrices are nx1 so we can multiply by 2
         size_t memBytes = sizeof(double) * (n*n + 2*n);
+        std::cout <<"\n" << std::endl;
         std::cout << "Bytes of Mem: " << memBytes << " " << std::endl;
+        //need to convert bytes to GiB -> bytes
+//https://www.dataunitconverter.com/byte-to-gibibyte
+        memBytes = memBytes / 1024 / 1024 /1024;
         double bandwidth = memBytes / elapsed.count();
         // 102 GiB peak bandwidth according to NERSC documentation
         //https://cplusplus.com/reference/ios/fixed/ for displaying more decimal points + precision command
         bandwidth = bandwidth / 102;
+        bandwidth = bandwidth * 100;
+        std::cout <<"\n" << std::endl;
         std::cout << "% of mem bandwidth: "<< std::fixed<<std::setprecision(5) << bandwidth << "  GiB/s" << std::endl;
-
-
+        std::cout <<"\n" << std::endl;
+//        double mflop = memBytes / 8;
+//         mflop = mflop / 1e-6;
+//         mflop = mflop / elapsed.count();
+//
+//        std::cout << "MFLOP/s: "<< std::fixed<<std::setprecision(5) << mflop << "MFLOP/s" << std::endl;
         // now invoke the cblas method to compute the matrix-vector multiply
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
 

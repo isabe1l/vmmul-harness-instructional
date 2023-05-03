fee859762000925f3eb80e24226c09df520b0910
Isabel <ifalcon@mail.sfsu.edu>
2023-05-02 18:21:04 -0700

    used wrong variable name for timer

--- benchmark.cpp
+++ benchmark.cpp
@@ -99,7 +99,7 @@ int main(int argc, char **argv) {
 
         // insert end timer code here, and print out the elapsed time for this problem size
         end_time = std::chrono::high_resolution_clock::now();
-        elapsed_time = end_time - start_time;
+        elapsed = end_time - start_time;
         std::cout << " Elapsed time: " << elapsed.count() << " " << std::endl;
 
         // now invoke the cblas method to compute the matrix-vector multiply

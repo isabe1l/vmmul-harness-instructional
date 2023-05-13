9a1df0cc3fd5abee616803a98b2844e30eca8c32
Isabel <ifalcon@mail.sfsu.edu>
2023-05-12 23:28:02 -0700

    added new files plus the code necessary for them

--- dgemv-openmp-inner.cpp
+++ dgemv-openmp-inner.cpp
@@ -14,24 +14,24 @@ const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
+#pragma omp parallel
+    {
+        int nthreads = omp_get_num_threads();
+        int thread_id = omp_get_thread_num();
 //      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-   }
+    }
 
-   // insert your dgemv code here. you may need to create additional parallel regions,
-   // and you may want to comment out the above parallel code block that prints out
-   // nthreads and thread_id to not taint your timings
+    // insert your dgemv code here. you may need to create additional parallel regions,
+    // and you may want to comment out the above parallel code block that prints out
+    // nthreads and thread_id to not taint your timings
     int rowOffset;
     //https://stackoverflow.com/questions/13357065/how-does-openmp-handle-nested-loops
     //used for collapse for nested loops
-#pragma omp parallel for collapse(2)
     for(int row = 0; row < n; row++){
         //rowOffset from lecture slides that talked about CP3
-        for(int col = 0; col < n; col++){
             rowOffset = row*n;
+#pragma omp parallel for reduction (+:y[row])
+        for(int col = 0; col < n; col++){
 //       printf("rowOffset: %d\n", rowOffset);
             y[row] = A[rowOffset + col] * x[col] + y[row];
 //           printf("y[%d]: %f\n", row, y[row]);
@@ -40,3 +40,6 @@ void my_dgemv(int n, double* A, double* x, double* y) {
 
 }
 
+//
+// Created by Isabel on 5/12/2023.
+//
